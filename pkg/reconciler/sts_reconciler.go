a370991210420e7cae7c50c860162b962b178839
zibai <zibai.gj@alibaba-inc.com>
2025-04-28 12:31:51 +0800

    change headless service ownereference from rbg to sts

--- pkg/reconciler/sts_reconciler.go
+++ pkg/reconciler/sts_reconciler.go
@@ -6,7 +6,6 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	appsapplyv1 "k8s.io/client-go/applyconfigurations/apps/v1"
@@ -34,6 +33,14 @@ func NewStatefulSetReconciler(scheme *runtime.Scheme, client client.Client) *Sta
 }
 
 func (r *StatefulSetReconciler) Reconciler(ctx context.Context, rbg *workloadsv1alpha1.RoleBasedGroup, role *workloadsv1alpha1.RoleSpec) error {
+	if err := r.reconcileStatefulSet(ctx, rbg, role); err != nil {
+		return err
+	}
+
+	return r.reconcileHeadlessService(ctx, rbg, role)
+}
+
+func (r *StatefulSetReconciler) reconcileStatefulSet(ctx context.Context, rbg *workloadsv1alpha1.RoleBasedGroup, role *workloadsv1alpha1.RoleSpec) error {
 	logger := log.FromContext(ctx)
 	logger.V(1).Info("start to reconciling sts workload")
 
@@ -74,12 +81,56 @@ func (r *StatefulSetReconciler) Reconciler(ctx context.Context, rbg *workloadsv1
 		return err
 	}
 
-	svcApplyConfig, err := r.constructServiceApplyConfiguration(ctx, rbg, role)
+	return nil
+}
+
+func (r *StatefulSetReconciler) reconcileHeadlessService(ctx context.Context, rbg *workloadsv1alpha1.RoleBasedGroup, role *workloadsv1alpha1.RoleSpec) error {
+	logger := log.FromContext(ctx)
+	logger.V(1).Info("start to reconciling headless service")
+
+	sts := &appsv1.StatefulSet{}
+	err := r.client.Get(ctx, types.NamespacedName{Name: rbg.GetWorkloadName(role), Namespace: rbg.Namespace}, sts)
+	if err != nil {
+		return fmt.Errorf("get sts error, skip reconcile svc. error:  %s", err.Error())
+	}
+
+	svcApplyConfig, err := r.constructServiceApplyConfiguration(ctx, rbg, role, sts)
 	if err != nil {
 		return err
 	}
+	obj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(svcApplyConfig)
+	if err != nil {
+		logger.Error(err, "Converting obj apply configuration to json.")
+		return err
+	}
 
-	return utils.PatchObjectApplyConfiguration(ctx, r.client, svcApplyConfig, utils.PatchSpec)
+	newSvc := &corev1.Service{}
+	if err = runtime.DefaultUnstructuredConverter.FromUnstructured(obj, newSvc); err != nil {
+		return fmt.Errorf("convert svcApplyConfig to svc error: %s", err.Error())
+	}
+
+	oldSvc := &corev1.Service{}
+	err = r.client.Get(ctx, types.NamespacedName{Name: rbg.GetWorkloadName(role), Namespace: rbg.Namespace}, oldSvc)
+	if err != nil && !apierrors.IsNotFound(err) {
+		return err
+	}
+
+	if oldSvc != nil {
+		equal, err := SemanticallyEqualService(oldSvc, newSvc)
+		if equal {
+			logger.V(1).Info("svc equal, skip reconcile")
+			return nil
+		}
+
+		logger.V(1).Info(fmt.Sprintf("svc not equal, diff: %s", err.Error()))
+	}
+
+	if err := utils.PatchObjectApplyConfiguration(ctx, r.client, svcApplyConfig, utils.PatchSpec); err != nil {
+		logger.Error(err, "Failed to patch svc apply configuration")
+		return err
+	}
+
+	return nil
 }
 
 func (r *StatefulSetReconciler) constructStatefulSetApplyConfiguration(
@@ -120,6 +171,7 @@ func (r *StatefulSetReconciler) constructServiceApplyConfiguration(
 	ctx context.Context,
 	rbg *workloadsv1alpha1.RoleBasedGroup,
 	role *workloadsv1alpha1.RoleSpec,
+	sts *appsv1.StatefulSet,
 ) (*coreapplyv1.ServiceApplyConfiguration, error) {
 	selectMap := map[string]string{
 		workloadsv1alpha1.SetNameLabelKey: rbg.Name,
@@ -133,12 +185,11 @@ func (r *StatefulSetReconciler) constructServiceApplyConfiguration(
 		WithLabels(rbg.GetCommonLabelsFromRole(role)).
 		WithAnnotations(rbg.GetCommonAnnotationsFromRole(role)).
 		WithOwnerReferences(metaapplyv1.OwnerReference().
-			WithAPIVersion(rbg.APIVersion).
-			WithKind(rbg.Kind).
-			WithName(rbg.Name).
-			WithUID(rbg.GetUID()).
-			WithBlockOwnerDeletion(true).
-			WithController(true),
+			WithAPIVersion(sts.APIVersion).
+			WithKind(sts.Kind).
+			WithName(sts.Name).
+			WithUID(sts.GetUID()).
+			WithBlockOwnerDeletion(true),
 		)
 	return serviceConfig, nil
 }
@@ -196,17 +247,8 @@ func (r *StatefulSetReconciler) CleanupOrphanedWorkloads(ctx context.Context, rb
 			if err := r.client.Delete(ctx, &sts); err != nil {
 				return fmt.Errorf("delete sts %s error: %s", sts.Name, err.Error())
 			}
+			// The deletion of headless services depends on its own reference
 			logger.Info("delete sts", "sts", sts.Name)
-
-			if err := r.client.Delete(ctx, &corev1.Service{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      sts.Spec.ServiceName,
-					Namespace: sts.Namespace,
-				},
-			}); err != nil {
-				return fmt.Errorf("delete svc %s error: %s", sts.Name, err.Error())
-			}
-			logger.Info("delete svc", "svc", sts.Name)
 		}
 	}
 	return nil
@@ -248,6 +290,26 @@ func statefulSetSpecEqual(spec1, spec2 appsv1.StatefulSetSpec) (bool, error) {
 
 	if equal, err := podTemplateSpecEqual(spec1.Template, spec2.Template); !equal {
 		return false, fmt.Errorf("podTemplateSpec not equal, %s", err.Error())
+	}
+
+	return true, nil
+}
+
+func SemanticallyEqualService(svc1, svc2 *corev1.Service) (bool, error) {
+	if svc1 == nil || svc2 == nil {
+		if svc1 != svc2 {
+			return false, fmt.Errorf("object is nil")
+		} else {
+			return true, nil
+		}
+	}
+
+	if equal, err := objectMetaEqual(svc1.ObjectMeta, svc2.ObjectMeta); !equal {
+		return false, fmt.Errorf("objectMeta not equal: %s", err.Error())
+	}
+
+	if !reflect.DeepEqual(svc1.Spec.Selector, svc2.Spec.Selector) {
+		return false, fmt.Errorf("selector not equal, old: %v, new: %v", svc1.Spec.Selector, svc2.Spec.Selector)
 	}
 
 	return true, nil
